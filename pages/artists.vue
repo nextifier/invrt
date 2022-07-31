b89700d87d25160dd724139cd584d60ac577b996
Nextifier <nextifier@gmail.com>
2022-07-31 20:11:23 +0700

    updates

--- pages/artists.vue
+++ pages/artists.vue
@@ -1,5 +1,18 @@
 <template>
-  <MeetTheArtists />
+  <!-- <MeetTheArtists /> -->
+  <div class="min-h-screen flex flex-col items-center justify-center">
+    <div class="-mt-20 flex flex-col items-center gap-6">
+      <span
+        class="w-px h-16 bg-gradient-to-b from-transparent to-primary-lighten"
+      ></span>
+      <h1 class="uppercase text-white tracking-[0.8em] text-center">
+        Coming soon
+      </h1>
+      <span
+        class="w-px h-16 bg-gradient-to-b from-primary-lighten to-transparent"
+      ></span>
+    </div>
+  </div>
 </template>
 
 <script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <Hero />
     <!-- <Teaser class="pt-16 lg:pt-24" /> -->
     <TheAwaited class="pt-24 lg:pt-32" />
-    <MeetTheArtists class="pt-24 lg:pt-32" />
+    <!-- <MeetTheArtists class="pt-24 lg:pt-32" /> -->
     <Gallery class="pt-24 lg:pt-32" />
     <GetTicketCTA class="pt-24 lg:pt-32" />
     <SeeYou class="py-32 lg:py-48" />
